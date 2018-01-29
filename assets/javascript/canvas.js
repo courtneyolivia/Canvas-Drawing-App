26948cf27faed1c0ca9fe058495bb590368edaaa
sakiohma <sakiohma@github.com>
2018-01-29 07:17:44 -0500

    added files

---
+++ assets/javascript/canvas.js
@@ -0,0 +1,37 @@
+var size = prompt('enter your brush size');
+var md = false;
+var canvas = document.getElementById('a');
+canvas.addEventListener('mousedown', down);
+canvas.addEventListener('mouseup', toggledraw);
+canvas.addEventListener('mousemove',
+	function(evt){
+		var mousePos = getMousePos(canvas, evt);
+		var posx = mousePos.x;
+		var posy = mousePos.y;
+		draw(canvas, posx, posy);
+	});
+
+function down(){
+	md = true;
+}
+
+function toggledraw(){
+	md = false;
+	canvas.style.curcor="default";
+}
+
+function getMousePos(canvas, evt){
+	var rect = canvas.getBoundingClientRect();
+	return{
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
+}
+
+function draw(canvas, posx, posy){
+	var context = canvas.getContext('2d');
+	if(md){
+		canvas.style.curcor="pointer";
+		context.fillRect(posx, posy, 4, 4);
+	}
+}
